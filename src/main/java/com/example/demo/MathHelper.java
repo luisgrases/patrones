b7ebdec56dacb1c0cdd4918f92e958af68b0b8a6
Luis <luisgrases@gmail.com>
2018-02-14 18:28:29 -0600

    Implementacion: RetornaLaSumaSiElStringContiene3oMasnumerosSeparadosPorComa

--- src/main/java/com/example/demo/MathHelper.java
+++ src/main/java/com/example/demo/MathHelper.java
@@ -6,7 +6,6 @@ public class MathHelper {
 
 	public int Add(String string) {
 		if (string == "") return 0;
-		if (string.split(",").length > 2) throw new IllegalArgumentException();
 		return Arrays.stream(string.split(",")).mapToInt(Integer::parseInt).sum();
 	}
 }
