0e57221f7cf33d9393a6d5f69af8da48f3b02f1a
Luis <luisgrases@gmail.com>
2018-02-14 18:34:56 -0600

    Implementacion: Retorna la suma aunque se usen : o ; como separadores:

--- src/main/java/com/example/demo/MathHelper.java
+++ src/main/java/com/example/demo/MathHelper.java
@@ -6,6 +6,6 @@ public class MathHelper {
 
 	public int Add(String string) {
 		if (string == "") return 0;
-		return Arrays.stream(string.split(",")).mapToInt(Integer::parseInt).sum();
+		return Arrays.stream(string.split(";|:|,")).mapToInt(Integer::parseInt).sum();
 	}
 }
