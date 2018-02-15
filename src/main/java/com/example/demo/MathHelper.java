7be6a39d85d429582ecdcec37d36d24f4bdc1b8f
Luis <luisgrases@gmail.com>
2018-02-14 18:05:36 -0600

    Implementacion: RetornaLaSumaSiElStringContiene2numerosSeparadosPorComa

--- src/main/java/com/example/demo/MathHelper.java
+++ src/main/java/com/example/demo/MathHelper.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 public class MathHelper {
 
 	public int Add(String string) {
-		return (string == "") ? 0 : Integer.parseInt(string);
+		if (string == "") return 0;
+		return Arrays.stream(string.split(",")).mapToInt(Integer::parseInt).sum();	
 	}
 }
